9469191d538e6707e3481f838e5749e617814501
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-06 11:11:15 +0800

    refactor: tweak

--- script/start-app-vps.sh
+++ script/start-app-vps.sh
@@ -8,7 +8,7 @@ set -x
 
 ps auxww | grep "--port 8001" | grep -v "grep"  | awk '{print $2}' | xargs -r kill -9
 
-pip3 install -r requirement.txt
+pip3 install -r requirements.txt
 
 nohup /usr/bin/python3.10 -m uvicorn main:app --reload --port 8001 --host 0.0.0.0 >> /opt/apps/chat/ai-hub/ai-hub.log &!
 
