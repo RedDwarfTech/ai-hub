5698622c44aeaa0f46b2113fd8aa4969c22365ae
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-06-25 13:11:07 +0800

    feat: tweak

--- src/config/openai/openai_config.py
+++ src/config/openai/openai_config.py
@@ -6,4 +6,4 @@ def set_openai_endpoint(api_key: str):
     openai.api_version = "2023-05-15"
     openai.api_type = "azure"
     openai.api_key = api_key
-    openai.api_base = os.environ.get('OPENAI_API_BASE')
+    openai.api_base = "https://reddwarfcv.openai.azure.com/"
